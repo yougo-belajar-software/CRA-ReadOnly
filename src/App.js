fbb0e2edca084812b6f98b6861f68e0fdbc255e6
Yougo Belajar Teknologi <yougo.batam@gmail.com>
2020-10-23 11:48:39 +0700

    update Styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,34 @@ import './App.css';
 import ListKaryawan from './ListKaryawan';
 import Karyawan from './Karyawan';
 import { useKaryawan } from './useKaryawan'
-import { Container } from '@material-ui/core/';
+import { Container, AppBar, Toolbar, Typography, Button, Link } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+import Image from 'material-ui-image'
 import ParticlesBg from 'particles-bg'
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: 20,
+    padding: 20
+  },
+  box: {
+    height: 50,
+    width:'100%'
+  },
+  appbar: {
+    marginBottom: 20
+  }
+}));
 
 const AnakKomponent = ({ Karyawans }) => (
   <React.Fragment>
@@ -19,12 +45,23 @@ const AnakKomponent = ({ Karyawans }) => (
 
 const App = () => {
   const [Karyawans] = useKaryawan([]);
-
+  const classes = useStyles();
 
   return (
     <div className="App">
-      <ParticlesBg type="random" bg={true} />
       <Container maxWidth="lg" component="h1">
+        <AppBar position="static" color="transparent" className={classes.appbar}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Daftar Karyawan Read Only
+    </Typography>
+            <Button>
+              <Link href="https://github.com/yougo-belajar-software/daftar-karyawan-cra" color="textPrimary">
+                <GitHubIcon />
+              </Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
         <ListKaryawan
           AnakKomponent={
             React.useMemo(
@@ -36,6 +73,8 @@ const App = () => {
             )}
         >
         </ListKaryawan>
+        <ParticlesBg type="cobweb" bg={true} />
+
       </Container>
     </div>
 
